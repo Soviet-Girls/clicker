706b6b1b9112b08fe3e973df04c73723cac9e2f8
buvanenko <gbuvanenko@gmail.com>
2024-04-13 12:42:23 +0300

    getOnlineStatus

--- main.py
+++ main.py
@@ -458,7 +458,9 @@ async def save_scores():
         await data.save_last_mines()
         await data.save_top()
         await widget.update()
-        await bot.api.groups.enable_online(group_id=225507433)
+        response = await bot.api.request("groups.getOnlineStatus", {"group_id": 225507433})
+        if response['response']["status"] != "online":
+            await bot.api.groups.enable_online(group_id=225507433)
     except Exception as e:
         print(f"Error saving scores: {e}")
         await bot.api.messages.send(
