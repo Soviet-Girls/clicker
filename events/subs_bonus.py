c34ddbfe4442fbd6a2e2ecf8cfd7d760991c7944
buvanenko <gleb@buvanenko.ru>
2024-04-19 17:09:44 +0300

    fix limits

--- events/subs_bonus.py
+++ events/subs_bonus.py
@@ -8,10 +8,13 @@ import data
 async def send(message: Message):
     users = await bot.api.groups.get_members(group_id=225507433)
     for user_id in users.items:
-        await data.change_score(user_id, 300000)
-        await bot.api.messages.send(
-            peer_id=user_id,
-            message="🎉 Вам начислен бонус 300 000 SG₽",
-            random_id=random.randint(0, 2 ** 64)
-        )
+        try:
+            await data.change_score(user_id, 300000)
+            await bot.api.messages.send(
+                peer_id=user_id,
+                message="🎉 Вам начислен бонус 300 000 SG₽",
+                random_id=random.randint(0, 2 ** 64)
+            )
+        except:
+            pass
     await message.answer("Бонус начислен всем участникам группы")
