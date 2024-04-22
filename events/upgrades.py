d173edd7201bec7b3f32ce8b137314b5b8cc9672
buvanenko <gleb@buvanenko.ru>
2024-04-22 12:11:43 +0300

    show level

--- events/upgrades.py
+++ events/upgrades.py
@@ -2,11 +2,16 @@ import random
 from vkbottle.bot import MessageEvent
 
 import keyboard
+import data
 from bot import bot
 
 # Обработка команды "🛍️  Улучшения"
 async def message(event: MessageEvent):
     user_id = event.object.peer_id
+    level = await data.get_level(user_id)
+    price, income = data.price_count(level)
+    bot_message = f'📊 Уровень: {level}\n'
+    bot_message += f'На следующем уровне: +{income} SG₽ за клик\n'
     bot_message = "Выбери улучшение:"
     await bot.api.messages.send(
         user_id=user_id,
