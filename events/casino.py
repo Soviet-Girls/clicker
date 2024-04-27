db7c841b829ca0418f468a998f0d9d7800da6da8
buvanenko <gleb@buvanenko.ru>
2024-04-27 10:08:03 +0300

    antispam casino

--- events/casino.py
+++ events/casino.py
@@ -1,4 +1,5 @@
 
+import time
 import random
 import asyncio
 from vkbottle.bot import MessageEvent
@@ -6,10 +7,49 @@ from vkbottle.bot import MessageEvent
 import logging
 
 import data
+from bot import bot, user_api
 
+first_clicks = {}
+spam_count = {}
 # Обработка команды "🎰 Казино"
 async def message(event: MessageEvent):
     user_id = event.object.peer_id
+    donut = await data.is_donut(user_id)
+    tm_last = await data.get_last_mine(user_id)
+    tm = int(time.time())
+    first_click = first_clicks.get(user_id, -1)
+    if first_click == -1:
+        first_clicks[user_id] = tm
+        first_click = tm
+
+    tm_diff = tm - tm_last
+    if tm_diff > 600:
+        first_clicks[user_id] = tm
+        first_click = tm
+
+    if donut:
+        time_ban_diff = 3600
+    else:
+        time_ban_diff = 1200
+    if tm - first_click > time_ban_diff:
+        spam_count[user_id] = spam_count.get(user_id, 0) + 1
+        if spam_count[user_id] > 50 and not donut:
+            await event.show_snackbar(f"⛔ {100-spam_count[user_id]} кликов до бана")
+        else:
+            await event.show_snackbar("⌛ Отвлекись на 10 минут")
+        if spam_count[user_id] > 99 and not donut:
+            await user_api.groups.ban(
+                group_id=225507433,
+                owner_id=user_id,
+                reason=0,
+                comment="Подозрительная активность. Обратитесь к администору.",
+                comment_visible=True
+            )
+            logging.info(f"[BAN] https://vk.com/gim225507433?sel={user_id}")
+            return
+        logging.info(f"[SPAM {spam_count[user_id]}/100] https://vk.com/gim225507433?sel={user_id}")
+        return
+
     score = await data.get_score(user_id)
     if score < 1000:
         await event.show_snackbar("🛑 Недостаточно средств!")
